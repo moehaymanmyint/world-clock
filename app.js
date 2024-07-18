4905ca3a2d6163994d84be6acee62337234305b9
moehaymanmyint <73158667+moehaymanmyint@users.noreply.github.com>
2024-07-18 12:08:26 +0630

    Add Multiple City

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
-setInterval(function(){
-    let cityElement = document.querySelector(".city");
-    let dateElemnt = document.querySelector(".date");
-    let timeElement = document.querySelector(".time");
+// Yangon
+let yangonCityElement = document.querySelector(".yangon-city");
+let yangonDateElemnt = document.querySelector(".yangon-date");
+let yangonTimeElement = document.querySelector(".yangon-time");
 
-    let yangonTimezone = moment().tz("Europe/Paris");
+let yangonTimezone = moment().tz("Asia/Yangon");
 
-    cityElement.innerHTML = "Paris";
-    dateElemnt.innerHTML = yangonTimezone.format("MMMM Do, YYYY");
-    timeElement.innerHTML = yangonTimezone.format("h:mm:ss A");
-}), 1000;
+yangonCityElement.innerHTML = "Yangon";
+yangonDateElemnt.innerHTML = yangonTimezone.format("MMMM Do, YYYY");
+yangonTimeElement.innerHTML = yangonTimezone.format("h:mm:ss A");
+
+// Bangkok
+let bangkokCityElement = document.querySelector(".bangkok-city");
+let bangkokDateElemnt = document.querySelector(".bangkok-date");
+let bangkokTimeElement = document.querySelector(".bangkok-time");
+
+let bangkokTimezone = moment().tz("Asia/Bangkok");
+
+bangkokCityElement.innerHTML = "Bangkok";
+bangkokDateElemnt.innerHTML = bangkokTimezone.format("MMMM Do, YYYY");
+bangkokTimeElement.innerHTML = bangkokTimezone.format("h:mm:ss A");
+
+function updateCity(event){
+    let cityName = event.target.options[event.target.selectedIndex].text;
+    let timezone = moment().tz(event.target.value);
+    let cityDisplayElement = document.querySelector(".city-display");
+    cityDisplayElement.innerHTML = `
+    <div class="city-diaply">
+        <div class="city-info">
+            <div class="details">
+                    <h1 class="city">${cityName}</h1>
+                    <p class="date">${timezone.format("MMMM Do, YYYY")}</p>
+                </div>
+            <div id="time" class="time">${timezone.format("h:mm:ss A")}</div>
+        </div>
+    </div>    
+    `;    
+}
+
+let cityList = document.querySelector(".city-list");
+cityList.addEventListener("change", updateCity);
+
