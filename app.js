34bb79adc886dc33cb14cc9c1556a8b4fd9ae045
moehaymanmyint <73158667+moehaymanmyint@users.noreply.github.com>
2024-07-18 17:16:05 +0630

    Added Local TimeZone

--- app.js
+++ app.js
@@ -1,28 +1,38 @@
-// Yangon
-let yangonCityElement = document.querySelector(".yangon-city");
-let yangonDateElemnt = document.querySelector(".yangon-date");
-let yangonTimeElement = document.querySelector(".yangon-time");
+function updateTime(){
+    // Yangon
+    let yangonCityElement = document.querySelector(".yangon-city");
+    let yangonDateElemnt = document.querySelector(".yangon-date");
+    let yangonTimeElement = document.querySelector(".yangon-time");
 
-let yangonTimezone = moment().tz("Asia/Yangon");
+    let yangonTimezone = moment().tz("Asia/Yangon");
 
-yangonCityElement.innerHTML = "Yangon";
-yangonDateElemnt.innerHTML = yangonTimezone.format("MMMM Do, YYYY");
-yangonTimeElement.innerHTML = yangonTimezone.format("h:mm:ss A");
+    yangonCityElement.innerHTML = "Yangon";
+    yangonDateElemnt.innerHTML = yangonTimezone.format("MMMM Do, YYYY");
+    yangonTimeElement.innerHTML = yangonTimezone.format("h:mm:ss A");
 
-// Bangkok
-let bangkokCityElement = document.querySelector(".bangkok-city");
-let bangkokDateElemnt = document.querySelector(".bangkok-date");
-let bangkokTimeElement = document.querySelector(".bangkok-time");
+    // Bangkok
+    let bangkokCityElement = document.querySelector(".bangkok-city");
+    let bangkokDateElemnt = document.querySelector(".bangkok-date");
+    let bangkokTimeElement = document.querySelector(".bangkok-time");
 
-let bangkokTimezone = moment().tz("Asia/Bangkok");
+    let bangkokTimezone = moment().tz("Asia/Bangkok");
 
-bangkokCityElement.innerHTML = "Bangkok";
-bangkokDateElemnt.innerHTML = bangkokTimezone.format("MMMM Do, YYYY");
-bangkokTimeElement.innerHTML = bangkokTimezone.format("h:mm:ss A");
+    bangkokCityElement.innerHTML = "Bangkok";
+    bangkokDateElemnt.innerHTML = bangkokTimezone.format("MMMM Do, YYYY");
+    bangkokTimeElement.innerHTML = bangkokTimezone.format("h:mm:ss A");
+}
 
-function updateCity(event){
-    let cityName = event.target.options[event.target.selectedIndex].text;
-    let timezone = moment().tz(event.target.value);
+function updateCity(event){ 
+    let timezone, cityName;
+    // Current Time Zone
+    if(event.target.value === "current"){
+        let currentTimezone = moment.tz.guess();
+        timezone = moment().tz(currentTimezone);
+        cityName =  currentTimezone.replace("_", " ").split("/")[1];
+    } else {
+        timezone = moment().tz(event.target.value);
+        cityName = event.target.options[event.target.selectedIndex].text;    
+    }
     let cityDisplayElement = document.querySelector(".city-display");
     cityDisplayElement.innerHTML = `
     <div class="city-diaply">
@@ -34,9 +44,11 @@ function updateCity(event){
             <div id="time" class="time">${timezone.format("h:mm:ss A")}</div>
         </div>
     </div>    
-    `;    
+    `;
 }
 
 let cityList = document.querySelector(".city-list");
 cityList.addEventListener("change", updateCity);
+
+setInterval(updateTime, 1000);
 
